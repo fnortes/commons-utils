c76b762b57719efabbeb7db977c51b724fd957f1
Francisco Nortes Belmonte <francisco.nortes@gmail.com>
2021-01-17 11:23:28 +0100

    Add new setup to package

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,6 @@
  * @param {any} value Is the any value to check.
  * @return {Boolean} True if the entry value is null or undefined. Else false.
  */
-export const isNull = (value: any): boolean => value === undefined || value === null;
+export const isNull = (value: any): boolean => {
+  return value === undefined || value === null;
+};
